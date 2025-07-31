d25b41ef7404d0d8251f8c04e7cb05060a7113c7
Kalafrak <mdelldeadstar@hotmail.co.uk>
2025-07-31 22:20:39 +0100

    add character count to stats.py and import to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import characters_used
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,7 +9,9 @@ def get_book_text(path_to_file):
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(file_contents)
+    characters = characters_used(file_contents)
     print(f"{word_count} words found in the document")
+    print(characters)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     word_list = text.split()
     return len(word_list)
+
+def characters_used(text):
+    text_lower = text.lower()
+    count = {}
+    for char in text_lower:
+        if char not in count:
+            count[char] = 1
+        else:
+             count[char] += 1
+    return count
